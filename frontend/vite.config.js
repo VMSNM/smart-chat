284a57000d540de287488c239afd079c5d652163
VMSNM <vitor.monteiro.84@gmail.com>
2024-09-30 00:54:36 +0100

    last-upd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://smart-chatapp.onrender.com",
+        target: "http://localhost:8000"
       }
     }
   }
